be2cd1094f465688b1a1cb337be340238a7fc987
thegerman <thegerman@thegerman.local>
2020-09-29 22:21:43 -0700

    Done-Deployment on Netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useLocalStorage from './hooks/useLocalStorage'
 
 import './App.css';
+import Editor from './components/Editor';
 
 function App() {
+
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(()=>{
+    const interval = setInterval(()=> {
+      setSrcDoc(`
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </hrml>
+      `)
+    },1000)
+    return () => clearInterval(interval)
+  }, [html, js, css])
+
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <div className='pane top-pane'>
+        <Editor 
+          language='xml' 
+          displayName='HTML' 
+          value={html} 
+          onChange={setHtml} />
+      <Editor 
+          language='css' 
+          displayName='CSS' 
+          value={css} 
+          onChange={setCss} />
+        <Editor 
+          language='javascript' 
+          displayName='Javascript' 
+          value={js} 
+          onChange={setJs} /> 
+      </div>
+      <div className='pane'>
+        <iframe
+        srcDoc={srcDoc}
+        title='output'
+        sandbox='allow-scripts'
+        frameBorder='0'
+        width='100%'
+        height='100%'
+        /> 
+      </div>
+    </>
   );
 }
 
